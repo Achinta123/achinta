eb8e7a2a0ab28f1e3c11eb23aaa7f8c702ba12e1
Achinta Das <achintadas13@gmail.com>
2018-12-11 03:42:20 -0800

    3. Delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,25 +1,31 @@
 pragma solidity ^0.4.2;
 
-contract DappToken{
-	string public name = "Vaisen";
-	string public symbol = "VIP";
-	string public standard = "VIP v 1.0";
-	uint256 public totalSupply;
+contract DappToken {
+    string  public name = "Vaisen";
+    string  public symbol = "VIP";
+    string  public standard = "VIP v1.0";
+    uint256 public totalSupply;
 
-	event Transfer(
-		address indexed _from, 
-		address indexed _to, 
-		uint256 _value );
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
-	mapping(address => uint256)public balanceOf;
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
-	function DappToken(uint256 _initialSupply)public{
-		balanceOf[msg.sender] = _initialSupply;
-		totalSupply = _initialSupply ;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-	}
-// Transfer Function  
-	
+    function DappToken (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
 
@@ -27,7 +33,29 @@ contract DappToken{
         balanceOf[_to] += _value;
 
         Transfer(msg.sender, _to, _value);
+
         return true;
     }
-	
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        Transfer(_from, _to, _value);
+
+        return true;
+    }
 }
