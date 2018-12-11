8efb90a44bd52c41963eaf173b559d4948df4b93
Achinta Das <achintadas13@gmail.com>
2018-12-10 22:56:58 -0800

    2. Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,33 @@
 pragma solidity ^0.4.2;
 
 contract DappToken{
-	//Constructor 
-	//set the total number of token
-	// Read The Total Number Token 
+	string public name = "Vaisen";
+	string public symbol = "VIP";
+	string public standard = "VIP v 1.0";
 	uint256 public totalSupply;
 
-	function DappToken()public{
-		totalSupply = 1000000;
+	event Transfer(
+		address indexed _from, 
+		address indexed _to, 
+		uint256 _value );
+
+	mapping(address => uint256)public balanceOf;
+
+	function DappToken(uint256 _initialSupply)public{
+		balanceOf[msg.sender] = _initialSupply;
+		totalSupply = _initialSupply ;
 
 	}
+// Transfer Function  
+	
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+	
 }
